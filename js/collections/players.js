bccc81509df7105bdea600ea4b3a6ac040e197ff
Simon Wood <simon.wood@holidayextras.com>
2013-02-26 20:43:23 +0000

    Changed user and users to player and player. Also changed player name to id.

--- js/collections/players.js
+++ js/collections/players.js
@@ -1,6 +1,6 @@
 /**
-* @name /js/model/users.js
-* @description Users Collection
+* @name /js/model/players.js
+* @description Player Collection
 * @author Simon Wood <hpoomdev@gamil.com>
 */
 
@@ -9,17 +9,17 @@ define( [
   'underscore',
   'backbone',
 	'moment',
-	'models/user',
+	'models/player',
 	'handlebars',
 	'hbs'
-], function( $, _, Backbone, moment, User ) {
-  var Users = Backbone.Collection.extend( {
-		model: User,
+], function( $, _, Backbone, moment, Player ) {
+  var Players = Backbone.Collection.extend( {
+		model: Player,
 		initialize: function ( params ) {
 			this.endpoint = params.endpoint;
 		},
 		url: function() {
-			// Get the user's data from SkyNet api
+			// Get the players data from SkyNet api
 			return 'http://skynet.nickg.org/' + this.endpoint;
 		},
 		comparator: function( collection ) {
@@ -28,11 +28,11 @@ define( [
 		parse: function( response ) {
 			response = _.pairs( response );
 			_.each( response, function( element, index, list ) {
-				list[index] = _.object( ['user', 'loginTime'], element );
+				list[index] = _.object( ['id', 'loginTime'], element );
 				list[index].timeOnline = moment( list[index].loginTime ).fromNow( true );
 			} );
 			return response;
 		}
 	} );
-  return Users;
+  return Players;
 } );
