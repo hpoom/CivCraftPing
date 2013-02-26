9c4b120f75118142a228711354bef5f54a2cf929
Simon Wood <simon.wood@holidayextras.com>
2013-02-26 15:54:30 +0000

    Added in timeOnline into the user model by handling it on the collection loading. Also updated the template to display this time.

--- js/collections/users.js
+++ js/collections/users.js
@@ -28,7 +28,13 @@ define( [
 			response = _.pairs( response );
 			_.each( response, function( element, index, list ) {
 				list[index] = _.object( ['user', 'loginTime'], element );
+				var dLoginTime = Date.parse( list[index].loginTime );
+				var dNow = new Date(); // Now does not consider time zones. Must add time zones in.
+				var buffer = 1000 * 60; // 60 second bufer to make sure times are not negative.
+				// Maybe just set negative numbers to zero???
+				list[index].timeOnline = Math.round( ( dNow.getTime() - dLoginTime ) / 1000 / 60 );
 			} );
+			console.log( response );
 			return response;
 		}
 	} );
